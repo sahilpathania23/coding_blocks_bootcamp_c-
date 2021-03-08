d1591a42244d1fb4bc0f988a012c03fb91a56e4c
sahil pathania <65562763+sahilpathania23@users.noreply.github.com>
2021-03-08 14:16:20 +0530

    Add files via upload
    wave in anticlockwise

--- wave_in_2dARRAY.cpp
+++ wave_in_2dARRAY.cpp
@@ -1,33 +1,33 @@
-#include <iostream>
-#include <climits>
-using namespace std;
+	#include <iostream>
+	#include <climits>
+	using namespace std;
 
-int main(){
-	
-	int m,n;
-	int arr[100][100];
-	cin>>m>>n;
-	for(int j=0;j<m;++j)
-	{
-		for(int y=0;y<n;++y){
-			cin>>arr[j][y];
+	int main(){
+		
+		int m,n;
+		int arr[100][100];
+		cin>>m>>n;
+		for(int j=0;j<m;++j)
+		{
+			for(int y=0;y<n;++y){
+				cin>>arr[j][y];
+			}
 		}
+		for(int cols=0;cols<n;++cols)
+		{
+			if (cols%2==0)
+			{
+				for(int rows=0;rows<m;++rows){
+					cout<<arr[rows][cols]<<","<<" ";
+				}
+			}else
+			{
+				for(int rows=m-1;rows>=0;--rows){
+					cout<<arr[rows][cols]<<","<<" ";
+				}
+				
+			}
+		} cout<<"END";
+	return 0;
+
 	}
-	for(int cols=0;cols<n;++cols)
-	{
-		if (cols%2==0)
-		{
-			for(int rows=0;rows<m;++rows){
-				cout<<arr[rows][cols]<<","<<" ";
-			}
-		}else
-		{
-			for(int rows=m-1;rows>=0;--rows){
-				cout<<arr[rows][cols]<<","<<" ";
-			}
-			
-		}
-	} cout<<"END";
-return 0;
-
-}
